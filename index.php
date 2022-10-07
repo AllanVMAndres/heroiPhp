a1aaff3bf988ffcc7cd332c80addd510d990d365
home <allan.vit2004@gmail.com>
2022-10-07 16:01:01 -0300

    v0.1

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method='post'>
+        <table>
+            <tr>
+                <td>Universo:</td>
+                <td>
+                    <input type="text" name="universo" size="50" placeholder="Marvel ou Dc">
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" id='txt'>Marvel</td>
+            </tr>
+            <tr>
+                <td>Heroi:</td>
+                <td>
+                    <input type="text" name="heroi-m" size="50" placeholder="Ciclope, Estigma, Hulk, Thor ou Vampira">
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" id='txt'>DC</td>
+            </tr>
+            <tr>
+                <td>Heroi:</td>
+                <td>
+                    <input type="text" name="heroi-d" size="50" placeholder="Aquaman, Batman, Ciborgue, Flash ou Superman">
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type="submit" value="Escolher">
+                </td>
+            </tr>
+        </table>
+    </form>
+    <?php
+    $uni = $_POST['universo'];
+
+    if ($uni == 'Marvel' || $uni == "marvel" || $uni == "MARVEL") {
+        $heroi = $_POST['heroi-m'];
+        if ($heroi == 'Ciclope' || $heroi == 'ciclope' || $heroi == 'CICLOPE') {
+            echo "<br><br><h3>".$heroi."</h3><img src='img/marvel-ciclope.jpg'>";
+        }
+        else if($heroi == 'Estigma' || $heroi == 'estigma' || $heroi == 'ESTIGMA'){
+            echo "<br><br><h3>".$heroi."</h3><img src='img/marvel-estigma.jpg'>";
+        }
+        else if($heroi == 'Hulk' || $heroi == 'hulk' || $heroi == 'HULK'){
+            echo "<br><br><h3>".$heroi."</h3><img src='img/marvel-hulk.jpg'>";
+        }
+        else if($heroi == 'Thor' || $heroi == 'thor' || $heroi == 'THOR'){
+            echo "<br><br><h3>".$heroi."</h3><img src='img/marvel-thor.jpg'>";
+        }
+        else if($heroi == 'Vampira' || $heroi == 'vampira' || $heroi == 'VAMPIRA'){
+            echo "<br><br><h3>".$heroi."</h3><img src='img/marvel-vampira.jpg'>";
+        }
+        else{
+            echo "<br><br>Inválido";
+        }
+    }
+    else if($uni == 'Dc' || $uni == 'dc' || $uni == 'DC'){
+        $heroi = $_POST['heroi-d'];
+
+        if($heroi == 'Aquaman' || $heroi == 'aquaman' || $heroi == 'AQUAMAN'){
+            echo "<br><br><h3>".$heroi."</h3><img src='img/dc-aquaman.jpg'>";
+        }
+        else if($heroi == 'Batman' || $heroi == 'batman' || $heroi == 'BATMAN'){
+            echo "<br><br><h3>".$heroi."</h3><img src='img/dc-batman.jpg'>";
+        }
+        else if($heroi == 'Ciborgue' || $heroi == 'ciborgue' || $heroi == 'CIBORGUE'){
+            echo "<br><br><h3>".$heroi."</h3><img src='img/dc-ciborgue.jpg'>";
+        }
+        else if($heroi == 'Flash' || $heroi == 'flash' || $heroi == 'FLASH'){
+            echo "<br><br><h3>".$heroi."</h3><img src='img/dc-flash.jpg'>";
+        }
+        else if($heroi == 'Superman' || $heroi == 'superman' || $heroi == 'SUPERMAN'){
+            echo "<br><br><h3>".$heroi."</h3><img src='img/dc-superman.jpg'>";
+        }
+        else{
+            echo "<br><br>Inválido";
+        }
+    }
+    else{
+        echo "<br><br>Inválido";
+    }
+    ?>
+</body>
+
+</html>
